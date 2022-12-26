213e14b296a66ec86223e24997a28d4054e8403a
NoorAlBattashi <nooralbattashi97@gmail.com>
2022-12-26 12:27:12 +0400

    latest version

--- src/stackQ.java
+++ src/stackQ.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * 
  */
@@ -12,8 +15,54 @@ public class stackQ {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String originString = "Somthing";
+		stackMethod(originString);
+		System.out.println();
+		System.out.println();
+		String Parentheses = "}";
+		calculateParenthesesWithStack(Parentheses);
+	}
 
+	public static void stackMethod(String origString) {
+		Stack<Character> addElements = new Stack<Character>();
+		for (int index = 0; index < origString.length(); index++) {
+
+			addElements.push(origString.charAt(index));
+		}
+		while (!addElements.empty()) {
+			System.out.print(addElements.pop());
+		}
+	}
+
+	public static void calculateParenthesesWithStack(String origString) {
+		Stack<Character> addElements = new Stack<Character>();
+		int countOpenParentheses = 0;
+		int countCloseParentheses = 0;
+		for (int index = 0; index < origString.length(); index++) {
+			char element = origString.charAt(index);
+			
+			if (element == '{') {
+				countOpenParentheses++;
+			} else if (element == '}') {
+				countCloseParentheses++;
+			}
+			
+			if (element == '{') {
+				addElements.push(element);
+			}
+
+			if (element == '}') {
+				if (!addElements.empty() && countOpenParentheses != 0) {
+					addElements.pop();
+				}
+			}
+		}
+		if (addElements.empty() && countOpenParentheses != 0 && countOpenParentheses == countCloseParentheses) {
+			System.out.println("valid");
+
+		} else {
+			System.out.println("Not valid");
+		}
 	}
 
 }
